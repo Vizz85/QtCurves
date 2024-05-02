fd08b3a63b09d023aa4c39566b9b6f824db45d64
Alessandro Vizin <alessandro.vizin@limango.com>
2024-05-02 12:02:37 +0200

    customize scale, interval and step count

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,9 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->ui->spinScale->setValue(this->ui->renderArea->scale());
+    this->ui->spinInterval->setValue(this->ui->renderArea->intervalLength());
+    this->ui->spinCount->setValue(this->ui->renderArea->stepCount());
 }
 
 MainWindow::~MainWindow()
@@ -13,10 +16,19 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::update_ui()
+{
+    this->ui->spinScale->setValue(this->ui->renderArea->scale());
+    this->ui->spinInterval->setValue(this->ui->renderArea->intervalLength());
+    this->ui->spinCount->setValue(this->ui->renderArea->stepCount());
+}
+
 void MainWindow::on_btnAstroid_clicked()
 {
     this->ui->renderArea->setShape(RenderArea::Astroid);
     this->ui->renderArea->repaint();
+
+    update_ui();
 }
 
 
@@ -24,6 +36,8 @@ void MainWindow::on_btnCycloid_clicked()
 {
     this->ui->renderArea->setShape(RenderArea::Cycloid);
     this->ui->renderArea->repaint();
+
+    update_ui();
 }
 
 
@@ -31,6 +45,8 @@ void MainWindow::on_btnHuygensCycloid_clicked()
 {
     this->ui->renderArea->setShape(RenderArea::HuygensCycloid);
     this->ui->renderArea->repaint();
+
+    update_ui();
 }
 
 
@@ -38,5 +54,25 @@ void MainWindow::on_btnHypoCycloid_clicked()
 {
     this->ui->renderArea->setShape(RenderArea::HypoCycloid);
     this->ui->renderArea->repaint();
+
+    update_ui();
+}
+
+
+void MainWindow::on_spinScale_valueChanged(double scale)
+{
+    this->ui->renderArea->setScale (scale);
+}
+
+
+void MainWindow::on_spinInterval_valueChanged(double intervalLength)
+{
+    this->ui->renderArea->setIntervalLength (intervalLength);
+}
+
+
+void MainWindow::on_spinCount_valueChanged(int count)
+{
+    this->ui->renderArea->setStepCount (count);
 }
 
